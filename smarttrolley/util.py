aaf87fd55c1567d013fa399a3012f3877a3b2ac5
Ujjawal Khadanga <b20139@students.iitmandi.ac.in>
2022-09-11 22:30:22 +0530

    Initial Commit

---
+++ smarttrolley/util.py
@@ -0,0 +1,58 @@
+import RPi.GPIO as GPIO
+GPIO.setwarnings(False)
+
+import os, time
+
+edgetpu=0 # If Coral USB Accelerator connected, then make it '1' otherwise '0'
+
+m1_1 = 8
+m1_2 = 11
+m2_1 = 14 
+m2_2 = 15 
+cam_light = 17
+headlight_right = 18
+headlight_left = 27 
+sp_light=9 
+
+
+def init_gpio():
+	GPIO.setmode(GPIO.BCM)
+	GPIO.setup(m1_1,GPIO.OUT)
+	GPIO.setup(m1_2,GPIO.OUT)
+	GPIO.setup(m2_1,GPIO.OUT)
+	GPIO.setup(m2_2,GPIO.OUT)
+	GPIO.setup(cam_light,GPIO.OUT)
+	GPIO.setup(headlight_right,GPIO.OUT)
+	GPIO.setup(headlight_left,GPIO.OUT)
+	GPIO.setup(sp_light,GPIO.OUT)
+	
+
+def back():
+    GPIO.output(m1_1, False)
+    GPIO.output(m1_2, True)
+    GPIO.output(m2_1, True)
+    GPIO.output(m2_2, False)
+    
+def right():
+	GPIO.output(m1_1, True)
+	GPIO.output(m1_2, False)
+	GPIO.output(m2_1, True)
+	GPIO.output(m2_2, False)
+
+def left():
+	GPIO.output(m1_1, False)
+	GPIO.output(m1_2, True)
+	GPIO.output(m2_1, False)
+	GPIO.output(m2_2, True)
+	
+def forward():
+	GPIO.output(m1_1, True)
+	GPIO.output(m1_2, False)
+	GPIO.output(m2_1, False)
+	GPIO.output(m2_2, True)
+	
+def stop():
+	GPIO.output(m1_1, False)
+	GPIO.output(m1_2, False)
+	GPIO.output(m2_1, False)
+	GPIO.output(m2_2, False)
